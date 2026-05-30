9fc1565883c8775e9d2916709d73da5d595dc008
Jeymicecilia <19912566@clases.edu.sv>
2026-05-30 12:51:54 -0600

    Create funciones2.py

---
+++ funciones2.py
@@ -0,0 +1,7 @@
+def saludar_y_sumar(nombre, num1, num2=0):
+    print("Saludos", nombre)
+    print("Resultado de la suma", num1+num2)
+
+saludar_y_sumar("Jeymi", 15, 9)
+saludar_y_sumar("Katya", 25)
+saludar_y_sumar("Yenifer", 26, 27)
