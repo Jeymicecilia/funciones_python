f3781d9a23444e7594d966732b5125e16d71ac01
Jeymicecilia <19912566@clases.edu.sv>
2026-05-30 12:46:34 -0600

    Create Tuplas.py

---
+++ Tuplas.py
@@ -0,0 +1,8 @@
+#Tuplas muy similares a las listas 
+#No pueden modificarse
+#Se utilizan parentesis en lugar de  {}
+
+lista = {"Jeymi", "Daysi"} #Constantemente agregado informacion
+tupla = {"Jeymi", "Daysi"}
+
+videojuegos = {  } 
