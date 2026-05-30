dceb5ce4fbd46fbd71aa61bccb13c8727cd0372d
Jeymicecilia <19912566@clases.edu.sv>
2026-05-30 12:48:17 -0600

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,14 @@
+#soy un comentario
+#strings, cadenas, textos
+#numericos int (intenger, entero) float (punto flotante)
+nombre = 'Jeymi'
+apellido = 'Zapata'
+edad = 16
+
+print(nombre,apellido,edad)
+
+#edad = edad /3
+
+print(nombre, apellido, edad)
+
+print(type(edad)) 
